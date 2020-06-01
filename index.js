fabd45d15a56d09d28d831c364cac0ea500c2fb5
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-06-01 15:30:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driverArray){
+    return driverArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(driverArray){
+    return driverArray.slice(-2);
+};
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(timesFare) {
+    return function (fare) {
+        return timesFare * fare;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(driverArray, selectFunction){
+    return selectFunction(driverArray);
+} 
